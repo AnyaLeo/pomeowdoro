98b603a955e6b6011125008e6082a810785a6df7
Anya <ania.leonova@gmail.com>
2020-02-08 10:18:10 -0800

    Added transparency + temporary placeholder for a cat

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+// Hot reload; to install, run "npm install --save-dev electron-reload" in your project if it doesnt work automatically
+require('electron-reload')(__dirname);
 
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    transparent: true,
+    frame: false,
     webPreferences: {
       nodeIntegration: true
     }
@@ -14,6 +18,7 @@ function createWindow () {
   win.loadFile('templates/index.html')
 
   // Open the DevTools.
+  // If the devtools are open, the window will not be transparent
   win.webContents.openDevTools()
 }
 
