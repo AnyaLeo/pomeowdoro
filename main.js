234755e77694fe919d1c854389dea1bb3dae6a6a
k <k>
2020-02-08 17:41:01 -0800

    countdown

--- main.js
+++ main.js
@@ -1,4 +1,60 @@
 const { app, BrowserWindow, screen } = require('electron');
+
+function Incrementor (workFunc, interval, errorFunc) {
+  var that = this;
+  var expected, timeout;
+  this.interval = interval;
+
+  this.start = function() {
+    expected = Date.now() + this.interval;
+    timeout = setTimeout(step, this.interval);
+  }
+
+  this.stop = function() {
+    clearTimeout(timeout);
+  }
+
+  function step() {
+    var drift = Date.now() - expected;
+    if (drift > that.interval) {
+      // You could have some default stuff here too...
+      if (errorFunc) errorFunc();
+    }
+    workFunc();
+    expected += that.interval;
+    timeout = setTimeout(step, Math.max(0, that.interval-drift));
+  }
+}
+
+function Time (seconds, minutes, hours) {
+  this.second = seconds;
+  this.minutes = minutes;
+  this.hours = hours;
+
+  this.totalSeconds = function() {
+    var sec = seconds + minutes * 60 + hours * 60 * 60;
+    return sec;
+  }
+
+  this.countDownFinished = function(countDownSeconds) {
+    return (this.totalSeconds() === countDownSeconds);
+  }
+
+  this.getSecond = function() {
+    return this.second;
+  }
+
+  this.getMinutes = function() {
+    return this.minutes;
+  }
+
+  this.getHours = function() {
+    return this.hours;
+  }
+
+}
+
+
 // Hot reload; to install, run "npm install --save-dev electron-reload" in your project if it doesnt work automatically
 require('electron-reload')(__dirname);
 
@@ -30,9 +86,32 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+// For testing purposes, we'll just increment
+// this and send it out to the console.
+  var justSomeNumber = 0;
+
+// Define the work to be done
+  var doWork = function() {
+    console.log(++justSomeNumber);
+  };
+
+// Define what to do if something goes wrong
+  var doError = function() {
+    console.warn('The drift exceeded the interval.');
+  };
+
+
+
+// (The third argument is optional)
+  var ticker = new Incrementor(doWork, 1000, doError);
+
+  ticker.start();
+
 
   // and load the index.html of the app.
   win.loadFile('templates/index.html')
+
+
 
   // Open the DevTools.
   // If the devtools are open, the window will not be transparent
