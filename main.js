5210995f51bcdf2731ec755b4331e2fefd235cf7
k <k>
2020-02-08 18:30:00 -0800

    timer working

--- main.js
+++ main.js
@@ -1,68 +1,11 @@
 const { app, BrowserWindow, screen } = require('electron');
 
-function Incrementor (workFunc, interval, errorFunc) {
-  var that = this;
-  var expected, timeout;
-  this.interval = interval;
-
-  this.start = function() {
-    expected = Date.now() + this.interval;
-    timeout = setTimeout(step, this.interval);
-  }
-
-  this.stop = function() {
-    clearTimeout(timeout);
-  }
-
-  function step() {
-    var drift = Date.now() - expected;
-    if (drift > that.interval) {
-      // You could have some default stuff here too...
-      if (errorFunc) errorFunc();
-    }
-    workFunc();
-    expected += that.interval;
-    timeout = setTimeout(step, Math.max(0, that.interval-drift));
-  }
-}
-
-function Time (seconds, minutes, hours) {
-  this.second = seconds;
-  this.minutes = minutes;
-  this.hours = hours;
-
-  this.totalSeconds = function() {
-    var sec = seconds + minutes * 60 + hours * 60 * 60;
-    return sec;
-  }
-
-  this.countDownFinished = function(countDownSeconds) {
-    return (this.totalSeconds() === countDownSeconds);
-  }
-
-  this.getSecond = function() {
-    return this.second;
-  }
-
-  this.getMinutes = function() {
-    return this.minutes;
-  }
-
-  this.getHours = function() {
-    return this.hours;
-  }
-
-}
-
-
 // Hot reload; to install, run "npm install --save-dev electron-reload" in your project if it doesnt work automatically
 require('electron-reload')(__dirname);
 
 // VARIABLES
 var appHeight = 200 + 30 + 50;
 var appWidth =  180;
-
-// FUNCTIONS
 
 function main() {
   createWindow();
@@ -86,27 +29,6 @@ function createWindow () {
       nodeIntegration: true
     }
   })
-// For testing purposes, we'll just increment
-// this and send it out to the console.
-  var justSomeNumber = 0;
-
-// Define the work to be done
-  var doWork = function() {
-    console.log(++justSomeNumber);
-  };
-
-// Define what to do if something goes wrong
-  var doError = function() {
-    console.warn('The drift exceeded the interval.');
-  };
-
-
-
-// (The third argument is optional)
-  var ticker = new Incrementor(doWork, 1000, doError);
-
-  ticker.start();
-
 
   // and load the index.html of the app.
   win.loadFile('templates/index.html')
@@ -118,8 +40,8 @@ function createWindow () {
   //win.webContents.openDevTools()
   document.getElementById("cat").style.width = "100px";
   document.getElementById("cat").style.height = "100px";
-}
 
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
---
+++ scripts/timer.js
@@ -0,0 +1,72 @@
+
+function Incrementor (workFunc, interval, errorFunc) {
+    var that = this;
+    var expected, timeout;
+    this.interval = interval;
+
+    this.start = function() {
+        expected = Date.now() + this.interval;
+        timeout = setTimeout(step, this.interval);
+    }
+
+    this.stop = function() {
+        clearTimeout(timeout);
+    }
+
+    function step() {
+        var drift = Date.now() - expected;
+        if (drift > that.interval) {
+            // You could have some default stuff here too...
+            if (errorFunc) errorFunc();
+        }
+        workFunc();
+        expected += that.interval;
+        timeout = setTimeout(step, Math.max(0, that.interval-drift));
+    }
+}
+
+function Time (seconds, minutes, hours) {
+    this.second = seconds;
+    this.minutes = minutes;
+    this.hours = hours;
+
+    this.totalSeconds = function() {
+        var sec = seconds + minutes * 60 + hours * 60 * 60;
+        return sec;
+    }
+
+    this.countDownFinished = function(countDownSeconds) {
+        return (this.totalSeconds() === countDownSeconds);
+    }
+
+    this.getSecond = function() {
+        return this.second;
+    }
+
+    this.getMinutes = function() {
+        return this.minutes;
+    }
+
+    this.getHours = function() {
+        return this.hours;
+    }
+
+}
+
+
+var justSomeNumber = 0;
+var timeSet = 20;
+
+var doError = function() {
+    console.warn('The drift exceeded the interval.');
+};
+
+// FUNCTIONS
+var doWork = function() {
+    document.getElementById('timer').textContent = "Timer: " + (timeSet - justSomeNumber);
+    console.log(++justSomeNumber);
+};
+
+var ticker = new Incrementor(doWork, 1000, doError);
+ticker.start();
+
